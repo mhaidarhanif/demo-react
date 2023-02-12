9227171e28bfce7470e8fa95540d03148b6ebe7e
M Haidar Hanif <git@mhaidarhanif.com>
2023-02-12 14:22:36 +0700

    feat: setup react app example

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Profile } from "./components/profile";
+
+export default function App() {
+  return (
+    <div>
+      <Profile />
+      <Profile />
+      <Profile />
+    </div>
+  );
+}
---
+++ src/components/profile.tsx
@@ -0,0 +1,8 @@
+export function Profile() {
+  return (
+    <div>
+      <h1>Elon Musk</h1>
+      <p>Bio profile story.</p>
+    </div>
+  );
+}
