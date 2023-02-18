673f24e747d33226405d2ec5a798232995e08615
M Haidar Hanif <git@mhaidarhanif.com>
2023-02-18 12:48:41 +0700

    feat: more contacts

--- src/data/contacts.ts
+++ src/data/contacts.ts
@@ -7,9 +7,13 @@ export type Contact = {
 export type Contacts = Contact[];
 
 export const dataContacts: Contact[] = [
-  { id: 1, name: "Rofiq", email: "name@example.com" },
-  { id: 2, name: "Jeffry", email: "name@example.com" },
-  { id: 3, name: "Bernhard", email: "name@example.com" },
+  { id: 1, name: "Rofiq", email: "rofiq@example.com" },
+  { id: 2, name: "Jeffry", email: "jeffry@example.com" },
+  { id: 3, name: "Bernhard", email: "bernhard@example.com" },
+  { id: 4, name: "Arif", email: "arif@example.com" },
+  { id: 5, name: "Farhan", email: "farhan@example.com" },
+  { id: 6, name: "Karim", email: "karim@example.com" },
+  { id: 7, name: "Abdehi", email: "abdehi@example.com" },
 ];
 
 export function getContacts() {
